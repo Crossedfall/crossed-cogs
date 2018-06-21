860fefaef93ec8c1b4e8e27a6fd9c28da293935f
Crossedfall <26130695+crossedfall@users.noreply.github.com>
2018-06-21 11:33:58 -0500

    First cog

---
+++ cakereact/__init__.py
@@ -0,0 +1,4 @@
+from .cakereact import CakeReact
+
+def setup(bot):
+    bot.add_cog(CakeReact(bot))
---
+++ cakereact/cakereact.py
@@ -0,0 +1,19 @@
+#Standard Imports
+import asyncio
+
+#Discord Imports
+from discord.ext import commands
+
+__version__ = "1.0.0"
+__author__ = "Crossedfall"
+
+class CakeReact:
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.cooldown(5, 60, commands.BucketType.channel)
+    async def on_message(self, message):
+        if message.author == self.bot.user:
+            return
+        if 'cake' in message.content.lower():
+            await message.add_reaction('🍰')
