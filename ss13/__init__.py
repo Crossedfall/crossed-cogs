23d3d115435ada10629ae0c8caa77f3993f3d65f
Crossedfall <26130695+crossedfall@users.noreply.github.com>
2018-09-12 22:48:01 -0500

    Starting point
    Starting from ground-0 baby

---
+++ ss13/__init__.py
@@ -0,0 +1,4 @@
+from .ss13 import SS13
+
+def setup(bot):
+    bot.add_cog(SS13(bot))
---
+++ ss13/ss13.py
@@ -0,0 +1,12 @@
+#Standard Imports
+import asyncio
+
+#Discord Imports
+from redbot.core import commands
+
+__version__ = "1.0.0"
+__author__ = "Crossedfall"
+
+class SS13:
+    def __init__(self, bot):
+        self.bot = bot
