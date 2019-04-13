920149095fe0f40cdc244ec38688d79ab3388655
Crossedfall <ping@crossedfall.io>
2019-04-12 20:43:45 -0500

    changes the `adminwho` world_topic request to comply with modern TG standards.

--- status/ss13status.py
+++ status/ss13status.py
@@ -258,7 +258,7 @@ class SS13Status(BaseCog):
         """
         server = await self.config.server()
         port = await self.config.game_port()
-        data = await self.query_server(server,port,"?adminWho")
+        data = await self.query_server(server,port,"?getAdmins")
 
         if data:
             try:
