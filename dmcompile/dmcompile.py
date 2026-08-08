50f21a7bea167ed9d852bf64d48bfaff39cac58f
Crossedfall <ping@crossedfall.io>
2026-08-08 13:05:10 -0500

    Forgot to include the code/version in the debug logging
    Add debug logging for error handling in dmcompile.

--- dmcompile/dmcompile.py
+++ dmcompile/dmcompile.py
@@ -123,6 +123,7 @@ class DMCompile(BaseCog):
                         r = r.json()
                 except (json.JSONDecodeError, httpx.ReadTimeout):
                     log.error("There was a problem with the response from the dmcompiler server", exc_info=True)
+                    log.debug(f"Message sent to compiler:\ncode:\n{code}\nversion:{version}")
                     embed = discord.Embed(description=f"There was a problem with the listener. Unable to retrieve any results!", color=0xff0000)
                     await ctx.send(embed=embed)
                     return await message.delete()
